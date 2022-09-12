18cc19e4d337f4da28259ab572dc40d92a4e5cdd
Karan <70828322+importKaran@users.noreply.github.com>
2022-09-12 20:17:23 +0530

    Update MaximumOf2Numbers.java

--- 02. First Java/MaximumOf2Numbers.java
+++ 02. First Java/MaximumOf2Numbers.java
@@ -7,10 +7,16 @@ public class MaximumOf2Numbers {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("\nProgram to convert INR to USD :");
-        System.out.println("Enter amount in Indian rupees : ");
-        float rupees = sc.nextFloat();
-        float usd = rupees / 79.37f;
-        System.out.println("Rs. " + rupees + " = $" + usd);
+        System.out.println("\nProgram to find maximum of the 2 user input numbers : ");
+        System.out.println("Enter 1st number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter 2nd number : ");
+        int b = sc.nextInt();
+        if(a > b)
+            System.out.println(a + " > " + b + "\n" + a + " is the greater number!");
+        else if(a < b)
+            System.out.println(a + " < " + b + "\n" + b + " is the greater number!");
+        else
+            System.out.println(a + " = " + b + "\nBoth numbers are equal!");
     }
 }
