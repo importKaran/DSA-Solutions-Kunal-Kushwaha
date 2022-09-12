0598b1207b539dca597260618f756683f9ba8a3e
Karan <70828322+importKaran@users.noreply.github.com>
2022-09-12 20:18:39 +0530

    Add files via upload

---
+++ 02. First Java/INRtoUSD.java
@@ -0,0 +1,16 @@
+package com.karan;
+
+import java.util.Scanner;
+
+public class INRtoUSD {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nProgram to convert INR to USD :");
+        System.out.println("Enter amount in Indian rupees : ");
+        float rupees = sc.nextFloat();
+        float usd = rupees / 79.37f;
+        System.out.println("Rs. " + rupees + " = $" + usd);
+    }
+}
